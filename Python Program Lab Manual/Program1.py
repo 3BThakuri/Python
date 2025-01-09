f749a52951f213b09610c12dd93ae4aa5335f361
3B Thakuri <bhaktabahadurbam.123549@marwadiuniversity.ac.in>
2025-01-09 22:13:27 +0530

    Add files via upload

---
+++ Python Program Lab Manual/Program1.py
@@ -0,0 +1,20 @@
+# prog for add,subs,multiply,divide two varible
+
+
+print("Enter two numbers")
+num1 = int(input("First number: "))
+num2 = int(input("Second number:"))
+
+add=num1+num2
+print('Addition is:',add)
+
+sub=num1-num2
+print('Subtraction is:',sub)
+
+mul=num1*num2
+print('Multiplication is:',mul)
+
+div=num1/num2
+print('Divisionis:',div)
+
+
