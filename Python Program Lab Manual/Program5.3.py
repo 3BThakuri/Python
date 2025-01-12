27a7e1d5a6d1feda2480f406bc219dfc97010bbf
3B Thakuri <2bbamthakuri@gmail.com>
2025-01-12 23:26:26 +0530

    Program5.3.py

---
+++ Python Program Lab Manual/Program5.3.py
@@ -0,0 +1,11 @@
+#check if a number is a multiple of 5 or Not
+
+num=int(input("Enter Number:"))
+
+if(num%5==0):
+        print('Entered Num is a multiple of 5')
+else:
+    print("Entered Number is Not a multiple of 5 ")
+
+
+
