5ee437426fc21b7cec9565296e179bc8ef569b94
3B Thakuri <2bbamthakuri@gmail.com>
2025-01-12 23:04:37 +0530

    Program5.2.py

---
+++ Python Program Lab Manual/Program5.2.py
@@ -0,0 +1,8 @@
+#check whether enter number is divisible by 7 or Not
+num=int(input("Enter Number:"))
+
+if(num%7==0):
+        print('Entered Num is divisible by 7')
+else:
+    print("Entered Num is Not divisible by 7")
+
