a289d0debadbb4124fc3892991e835c9746b44ac
3B Thakuri <2bbamthakuri@gmail.com>
2025-01-12 22:50:18 +0530

    Program5.1.py

--- Python Program Lab Manual/Program5.1.py
+++ Python Program Lab Manual/Program5.1.py
@@ -1,7 +1,6 @@
 # check if a person is eligible to vote
 age=int(input("Enter your age: "))
-
-if age >=18: 
+if age>=18: 
     print("You are eligible to vote.")
 else: 
     print("You are not eligible to vote.")
