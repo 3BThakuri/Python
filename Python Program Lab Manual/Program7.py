b1b4912d5475d95737a55ae78f8340e8d0e3aca0
Bhakta Bam <183859583+3BThakuri@users.noreply.github.com>
2025-01-21 19:24:38 +0530

    Program7.py

--- Python Program Lab Manual/Program7.py
+++ Python Program Lab Manual/Program7.py
@@ -1,1 +1,26 @@
-#Coming Soon
+#check given number prime or not
+n=int(input("Enter Number: "))
+
+
+
+f=0
+
+for i in range(2, n):
+
+        if (n % i) == 0:
+
+            f=1
+
+           
+
+            break
+
+
+
+if f==1:
+
+        print("not prime number")
+
+else:
+
+    print(" prime number")
