091aa66a6e3bb9f04faa2f0add22f4da34dcec35
Bhakta Bam <183859583+3BThakuri@users.noreply.github.com>
2025-02-06 22:44:59 +0530

    Program9.py

--- Python Program Lab Manual/Program9.py
+++ Python Program Lab Manual/Program9.py
@@ -1,3 +1,9 @@
-#Comming Soon............
-....
- 
+# write a program to calculate sum of all numbers from 1 to given number
+
+n=int(input("enter your number"))
+
+sum=0
+for x in range(1,n+1):
+    sum=sum+x
+
+print(sum)
