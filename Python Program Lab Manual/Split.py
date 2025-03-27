52a3f7c165168edb1c1112c68b4669af36691f0a
Bhakta Bam <183859583+3BThakuri@users.noreply.github.com>
2025-03-27 23:35:42 +0530

    Split.py

---
+++ Python Program Lab Manual/Split.py
@@ -0,0 +1,22 @@
+# program  for split 
+
+n=input("enter value with comma")
+
+x=n.split(",")
+
+print(x)
+
+# program for rsplit
+
+n=input("enter value with comma")
+
+x=n.rsplit(",")
+
+print(x)
+
+#program for splitlines
+n=input("enter value with comma")
+
+x=n.splitlines()
+
+print(x)
